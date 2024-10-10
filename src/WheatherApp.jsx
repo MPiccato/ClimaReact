05a254b2e2ffbfc608088f15a79f9c0e3628058b
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2024-10-10 08:06:57 -0300

    lee bien el input

--- src/WheatherApp.jsx
+++ src/WheatherApp.jsx
@@ -1,17 +1,50 @@
+import { useState } from 'react'
 import './styles/WheaterApp.css'
 
 export const WheatherApp = () => {
 
+    const urlBase = `https://api.openweathermap.org/data/2.5/weather?` //q=${city}&appid=${apiKey}&lang=es`
+    const apiKey = '03a6c4949511ea2cd914f615bd8a1e4c'
+
+    const [city, setCity] = useState('')
+    const [weatherData, setWeatherData] = useState(null);
+
+
+
+    const fetchWeatherData = async () => {
+
+        try {
+            const response = await fetch(`${urlBase}q=${city}&appid=${apiKey}`)
+            const data = await response.json()
+            setWeatherData(data)
+        } catch (error) {
+            console.error(error)
+
+        }
+
+
+    }
+
+    const handleCityChange = (e) => {
+        setCity(e.target.value)
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('Hola mundo')
+        console.log(city)
+        //fetchWeatherData()
     }
+
     return (
         <div className='container'>
             <h1>Aplicación del Clima</h1>
             <form onSubmit={handleSubmit}>
 
-                <input type="text" placeholder='Ingrese ciudad' />
+                <input
+                    type="text"
+                    placeholder='Ingrese ciudad'
+                    value={city}
+                    onChange={handleCityChange} />
                 <button type='submit'>Buscar</button>
             </form>
 
