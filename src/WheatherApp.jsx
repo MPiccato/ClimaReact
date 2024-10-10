854d1c3c13fc526b506f25c0ce2307c5ffa99f7f
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2024-10-10 18:23:27 -0300

    app terminada

--- src/WheatherApp.jsx
+++ src/WheatherApp.jsx
@@ -5,16 +5,18 @@ export const WheatherApp = () => {
 
     const urlBase = `https://api.openweathermap.org/data/2.5/weather?`
     const apiKey = '03a6c4949511ea2cd914f615bd8a1e4c'
+    const diffKelvin = 273.15
 
     const [city, setCity] = useState('')
     const [weatherData, setWeatherData] = useState(null);
 
 
 
+    // Función que llama a la API
     const fetchWeatherData = async () => {
 
         try {
-            const response = await fetch(`${urlBase}q=${city}&appid=${apiKey}`)
+            const response = await fetch(`${urlBase}q=${city}&appid=${apiKey}&lang=es`)
             const data = await response.json()
 
             setWeatherData(data)
@@ -26,10 +28,15 @@ export const WheatherApp = () => {
 
     }
 
+
+    //    Toma el valor que tiene el input y se lo asigna a city
     const handleCityChange = (e) => {
         setCity(e.target.value)
     }
 
+
+
+    // Función del Formulario que ejecuta el llamado
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -51,7 +58,11 @@ export const WheatherApp = () => {
 
             {weatherData && (
                 <div>
-                    <h2>{weatherData.name}</h2>
+                    <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+                    <p>La temperatura actual es: {Math.floor(weatherData.main.temp - diffKelvin)} C </p>
+                    <p>{weatherData.weather[0].description}</p>
+                    <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather[0].description} />
+
                 </div>
             )}
 
